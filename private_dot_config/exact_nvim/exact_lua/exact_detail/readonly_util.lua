4c42202f44b5af3972995f53968653448b898c6e
William Roy <wroy@proton.me>
2023-10-31 10:31:59 -0400

    fix(nvim): inverse inlay true false condition

--- private_dot_config/exact_nvim/exact_lua/exact_detail/readonly_util.lua
+++ private_dot_config/exact_nvim/exact_lua/exact_detail/readonly_util.lua
@@ -5,19 +5,17 @@ local M = {}
 -- @param buffer integer
 function M.try_add_inlay(client, buffer)
   if client.supports_method([[textDocument/inlayHint]]) then
-    --  Protected call as supports_method is not always sufficient.
-    --
     pcall(vim.lsp.inlay_hint, buffer, true)
     vim.api.nvim_create_autocmd({ [[InsertEnter]] }, {
       buffer = buffer,
       callback = function()
-        pcall(vim.lsp.inlay_hint, buffer, true)
+        pcall(vim.lsp.inlay_hint, buffer, false)
       end
     })
     vim.api.nvim_create_autocmd({ [[InsertLeave]] }, {
       buffer = buffer,
       callback = function()
-        pcall(vim.lsp.inlay_hint, buffer, false)
+        pcall(vim.lsp.inlay_hint, buffer, true)
       end
     })
   end
