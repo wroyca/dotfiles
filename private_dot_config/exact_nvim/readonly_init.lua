09ca49a0aa2db90ae86b22a16038bc27d4cc44a4
William Roy <wroy@proton.me>
2023-11-27 19:18:15 -0500

    feat(nvim): defer opts and shada

--- private_dot_config/exact_nvim/readonly_init.lua
+++ private_dot_config/exact_nvim/readonly_init.lua
@@ -5,66 +5,85 @@ local _ = string.format([[
 (  /  |        / | \                                    * .  ' .  .-+-
  \(_)_%s      /  |  \                                *   *  .   .]], "]]")
 
-local h = io.popen([[
-gdbus call --session \
-           --dest=org.freedesktop.portal.Desktop \
-           --object-path=/org/freedesktop/portal/desktop \
-           --method=org.freedesktop.portal.Settings.Read org.freedesktop.appearance color-scheme
-]])
+vim.schedule(function()
+  local h = io.popen([[
+  gdbus call --session \
+             --dest=org.freedesktop.portal.Desktop \
+             --object-path=/org/freedesktop/portal/desktop \
+             --method=org.freedesktop.portal.Settings.Read org.freedesktop.appearance color-scheme
+  ]])
 
-if h ~= nil then
-  if string.match(h:read('*a'), ' %d') == " 1" then
-    h:close()
-    vim.cmd [[colorscheme dark]]
-  else
-    h:close()
-    vim.cmd [[colorscheme light]]
+  if h ~= nil then
+    if string.match(h:read('*a'), ' %d') == " 1" then
+      h:close()
+      vim.cmd [[colorscheme dark]]
+    else
+      h:close()
+      vim.cmd [[colorscheme light]]
+    end
   end
-end
+end)
+
+local shada = vim.o.shada
+vim.o.shada = [[]]
+vim.api.nvim_create_autocmd([[User]], {
+  pattern = [[VeryLazy]],
+  callback = function()
+    vim.o.shada = shada
+    pcall(vim.cmd.rshada, { bang = true })
+  end,
+})
 
 vim.g.mapleader      = [[ ]]
 vim.g.localmapleader = [[,]]
-vim.o.clipboard      = [[unnamedplus]]
-vim.o.fileencoding   = [[utf-8]]
-vim.o.whichwrap      = [[b,s,<,>,h,l]]
-vim.o.signcolumn     = [[yes:1]]
-vim.o.virtualedit    = [[onemore]]
-vim.o.titlestring    = [[%t]]
-vim.o.splitkeep      = [[screen]]
-vim.o.showmode       = false
-vim.o.number         = false
-vim.o.relativenumber = false
-vim.wo.wrap          = true
-vim.o.fsync          = true
-vim.o.title          = true
-vim.o.confirm        = true
-vim.o.splitbelow     = true
-vim.o.splitright     = true
-vim.o.termguicolors  = true
-vim.o.undofile       = true
-vim.o.expandtab      = true
-vim.o.autoindent     = true
-vim.o.breakindent    = true
-vim.o.smartindent    = true
-vim.o.smarttab       = true
-vim.o.preserveindent = true
-vim.o.cursorline     = true
-vim.o.conceallevel   = 3
-vim.o.winblend       = 0
-vim.o.tabstop        = 2
-vim.o.shiftwidth     = 2
-vim.o.scrolloff      = 4
-vim.o.pumheight      = 5
-vim.o.cmdheight      = 0
-vim.o.synmaxcol      = 0
-vim.o.laststatus     = 0
-vim.o.foldlevel      = 99
-vim.o.foldlevelstart = 99
-vim.o.foldenable     = true
-vim.o.list           = true
-vim.opt.suffixes     = vim.opt.suffixes - [[.h]]
-vim.opt.cinkeys      : remove([[:]])
-vim.opt.indentkeys   : remove([[:]])
+vim.api.nvim_create_autocmd(
+  [[UiEnter]],
+{
+  once = true,
+  callback = function()
+    vim.o.clipboard      = [[unnamedplus]]
+    vim.o.fileencoding   = [[utf-8]]
+    vim.o.whichwrap      = [[b,s,<,>,h,l]]
+    vim.o.signcolumn     = [[yes:1]]
+    vim.o.virtualedit    = [[onemore]]
+    vim.o.titlestring    = [[%t]]
+    vim.o.splitkeep      = [[screen]]
+    vim.o.showmode       = false
+    vim.o.number         = false
+    vim.o.relativenumber = false
+    vim.wo.wrap          = true
+    vim.o.fsync          = true
+    vim.o.title          = true
+    vim.o.confirm        = true
+    vim.o.splitbelow     = true
+    vim.o.splitright     = true
+    vim.o.termguicolors  = true
+    vim.o.undofile       = true
+    vim.o.expandtab      = true
+    vim.o.autoindent     = true
+    vim.o.breakindent    = true
+    vim.o.smartindent    = true
+    vim.o.smarttab       = true
+    vim.o.preserveindent = true
+    vim.o.cursorline     = true
+    vim.o.conceallevel   = 3
+    vim.o.winblend       = 0
+    vim.o.tabstop        = 2
+    vim.o.shiftwidth     = 2
+    vim.o.scrolloff      = 4
+    vim.o.pumheight      = 5
+    vim.o.cmdheight      = 0
+    vim.o.synmaxcol      = 0
+    vim.o.laststatus     = 0
+    vim.o.foldlevel      = 99
+    vim.o.foldlevelstart = 99
+    vim.o.foldenable     = true
+    vim.o.list           = true
+    vim.opt.suffixes     = vim.opt.suffixes - [[.h]]
+    vim.opt.cinkeys      : remove([[:]])
+    vim.opt.indentkeys   : remove([[:]])
+  end
+})
 
 local lazypath = vim.fn.stdpath([[data]]) .. [[/lazy/lazy.nvim]]
 if not vim.loop.fs_stat(lazypath) then
