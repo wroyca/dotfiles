b229c56442d7256a4680ac09d9987433ac392680
William Roy <wroy@proton.me>
2023-11-30 09:40:54 -0500

    feat(nvim): swap mini.comment with comment.nvim

--- private_dot_config/exact_nvim/exact_lua/exact_spec/exact_mini/readonly_mini-comment.lua
+++ private_dot_config/exact_nvim/exact_lua/exact_spec/exact_mini/readonly_mini-comment.lua
@@ -1,5 +1,19 @@
 ---@type LazyPluginSpec
 return {
-  [[echasnovski/mini.comment]],
-  enabled = false
+  ---@diagnostic disable-next-line: assign-type-mismatch
+  {
+    [[echasnovski/mini.comment]],
+    enabled = false
+  },
+  {
+    [[numToStr/Comment.nvim]],
+    keys = {
+      { [[gc]], mode = { [[n]], [[v]] }, desc = [[Comment toggle linewise]] },
+      { [[gb]], mode = { [[n]], [[v]] }, desc = [[Comment toggle blockwise]] },
+    },
+    opts = function(_, opts)
+      local has_cs, cs = pcall(require, [[ts_context_commentstring.integrations.comment_nvim]])
+      if has_cs then opts.pre_hook = cs.create_pre_hook() end
+    end
+  }
 }
