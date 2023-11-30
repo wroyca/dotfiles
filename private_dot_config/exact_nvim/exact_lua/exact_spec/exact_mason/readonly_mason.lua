eb6417da8baf5508eafb236dfdf98484ba4725ec
William Roy <wroy@proton.me>
2023-11-30 08:35:43 -0500

    feat(nvim): force ensure_installed with mason

--- private_dot_config/exact_nvim/exact_lua/exact_spec/exact_mason/readonly_mason.lua
+++ private_dot_config/exact_nvim/exact_lua/exact_spec/exact_mason/readonly_mason.lua
@@ -2,5 +2,27 @@
 return {
   [[williamboman/mason.nvim]],
   name = [[mason]],
+  build = function()
+    ---@diagnostic disable-next-line: undefined-field
+    require [[mason]].setup()
+    local mr = require [[mason-registry]]
+    local function ensure_installed()
+      for _, tool in ipairs({
+        [[clangd]],
+        [[lua-language-server]]
+      })
+      do
+        local p = mr.get_package(tool)
+        if not p:is_installed() then
+          p:install()
+        end
+      end
+    end
+    if mr.refresh then
+      mr.refresh(ensure_installed)
+    else
+      ensure_installed()
+    end
+  end,
   config = true
 }
