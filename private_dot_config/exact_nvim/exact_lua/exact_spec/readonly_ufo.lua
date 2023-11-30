46baac16cb2e8245c71e22bd70e88961e3ecd169
William Roy <wroy@proton.me>
2023-11-30 08:38:21 -0500

    feat(nvim): remove ufo keymap

--- private_dot_config/exact_nvim/exact_lua/exact_spec/readonly_ufo.lua
+++ private_dot_config/exact_nvim/exact_lua/exact_spec/readonly_ufo.lua
@@ -1,35 +1,23 @@
+---@type LazyPluginSpec
 return {
   [[kevinhwang91/nvim-ufo]],
+  name = [[ufo]],
   event = [[VeryLazy]],
-  dependencies = [[kevinhwang91/promise-async]],
-
   opts = {
-    preview = {
-      mappings = {
-        scrollB = [[<C-b>]],
-        scrollF = [[<C-f>]],
-        scrollU = [[<C-u>]],
-        scrollD = [[<C-d>]]
-      }
-    },
-
     provider_selector = function(_, filetype, buftype)
       local function handleFallbackException(bufnr, err, providerName)
-        if type(err) == "string" and err:match "UfoFallbackException" then
-          return require("ufo").getFolds(bufnr, providerName)
+        if type(err) == [[string]] and err:match [[UfoFallbackException]] then
+          return require [[ufo]].getFolds(bufnr, providerName)
         else
-          return require("promise").reject(err)
+          return require [[promise]].reject(err)
         end
       end
-
       return (filetype == [[]] or buftype == [[nofile]]) and [[indent]] or function(bufnr)
-        return require [[ufo]].getFolds(bufnr, [[lsp]])
-            :catch(function(err)
-              return handleFallbackException(bufnr, err, [[treesitter]])
-            end)
-            :catch(function(err)
-              return handleFallbackException(bufnr, err, [[indent]])
-            end)
+        return require [[ufo]].getFolds(bufnr, [[lsp]]):catch(function(err)
+          return handleFallbackException(bufnr, err, [[treesitter]])
+        end):catch(function(err)
+          return handleFallbackException(bufnr, err, [[indent]])
+        end)
       end
     end
   }
