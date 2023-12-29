8b114d28ab77135e7e003f39683e74e64641b6d0
William Roy <wroy@proton.me>
2023-12-29 18:39:52 -0500

    feat(nvim): autocmd helper

--- private_dot_config/exact_nvim/readonly_init.lua
+++ private_dot_config/exact_nvim/readonly_init.lua
@@ -21,6 +21,38 @@ if h ~= nil then
     vim.o.background = [[light]]
   end
 end
+vim.api.nvim_create_autocmd([[VimResized]], {
+  group = vim.api.nvim_create_augroup([[resize_splits]], { clear = true }),
+  callback = function()
+    local current_tab = vim.fn.tabpagenr()
+    vim.cmd([[tabdo wincmd =]])
+    vim.cmd([[tabnext ]] .. current_tab)
+  end
+})
+
+vim.api.nvim_create_autocmd([[FileType]], {
+  group = vim.api.nvim_create_augroup([[close_with_q]], { clear = true }),
+  pattern = {
+    [[PlenaryTestPopup]],
+    [[help]],
+    [[lspinfo]],
+    [[man]],
+    [[notify]],
+    [[qf]],
+    [[query]],
+    [[spectre_panel]],
+    [[startuptime]],
+    [[tsplayground]],
+    [[neotest-output]],
+    [[checkhealth]],
+    [[neotest-summary]],
+    [[neotest-output-panel]],
+  },
+  callback = function(event)
+    vim.bo[event.buf].buflisted = false
+    vim.keymap.set([[n]], [[q]], [[<cmd>close<cr>]], { buffer = event.buf, silent = true })
+  end,
+})
 
 local shada = vim.o.shada
 vim.o.shada = [[]]
@@ -59,7 +91,6 @@ vim.o.smartindent    = true
 vim.o.smarttab       = true
 vim.o.preserveindent = true
 vim.o.cursorline     = true
-vim.o.conceallevel   = 3
 vim.o.winblend       = 0
 vim.o.tabstop        = 2
 vim.o.shiftwidth     = 2
