bfb71a9ea37d253fac2cff00b060a75de0c78f92
William Roy <wroy@proton.me>
2024-03-20 18:54:36 -0400

    Add firefox profile

---
+++ linux/private_dot_mozilla/exact_firefox/default-release/user.js
@@ -0,0 +1,10 @@
+// Mozilla User Preferences
+
+user_pref("gnomeTheme.hideSingleTab", true);
+user_pref("gnomeTheme.bookmarksToolbarUnderTabs", true);
+user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);
+user_pref("browser.uidensity", 0);
+user_pref("svg.context-properties.content.enabled", true);
+user_pref("browser.theme.dark-private-windows", false);
+user_pref("widget.gtk.rounded-bottom-corners.enabled", true);
+user_pref("browser.uiCustomization.state", "{\"placements\":{\"widget-overflow-fixed-list\":[],\"unified-extensions-area\":[],\"nav-bar\":[\"back-button\",\"forward-button\",\"stop-reload-button\",\"new-tab-button\",\"customizableui-special-spring1\",\"urlbar-container\",\"customizableui-special-spring2\",\"save-to-pocket-button\",\"downloads-button\",\"fxa-toolbar-menu-button\",\"unified-extensions-button\"],\"toolbar-menubar\":[\"menubar-items\"],\"TabsToolbar\":[\"tabbrowser-tabs\",\"alltabs-button\"],\"PersonalToolbar\":[\"personal-bookmarks\"]},\"seen\":[\"save-to-pocket-button\",\"developer-button\"],\"dirtyAreaCache\":[\"nav-bar\",\"toolbar-menubar\",\"TabsToolbar\",\"PersonalToolbar\"],\"currentVersion\":20,\"newElementCount\":3}");
