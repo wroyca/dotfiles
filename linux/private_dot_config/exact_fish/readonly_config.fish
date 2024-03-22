95a2369285a53ab29f61a530ada104c30f15240c
William Roy <wroy@proton.me>
2024-03-22 12:33:43 -0400

    Fix fish prompt and use fish_add_path

--- linux/private_dot_config/exact_fish/readonly_config.fish
+++ linux/private_dot_config/exact_fish/readonly_config.fish
@@ -1,31 +1,27 @@
-source ~/.config/fish/fish_aliases.fish
-
-# Bind Ctrl-Z to FG
-bind \cz 'fg 2>/dev/null; commandline -f repaint'
-
-# Unlike other shells, there is no prompt variable like PS1. To display our
-# prompt, fish executes the fish_prompt function and uses its output as the
-# prompt. And if it exists, fish also executes the fish_right_prompt function
-# and uses its output as the right prompt.
-#
 function fish_prompt
   set -l cwd (pwd | string replace "$HOME" '~')
   set -l branch (git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/ (\1)/')
 
-  # Show an icon when inside a container, such as a toolbox or distrobox.
-  #
-  if test -f /run/.containerenv
-    echo -n (set_color magenta)⬢(set_color normal)
-  end
-
   # [user@hostname ~]$ ...
+  #
   echo -n '['
-    echo -n (whoami)'@'(hostname) (basename "$cwd")"$git_branch"
+    echo -n (whoami)'@'(hostname) (basename "$cwd")"$branch"
   echo -n ']$ '
 end
 
-
-# Suppress greeting message printed on startup.
-#
 function fish_greeting
 end
+
+# Source bash aliases, as simpler ones are compatible with Fish.
+#
+source ~/.config/fish/fish_aliases.fish
+
+# Bind `ctrl-z` to resume suspended jobs in the background and foreground.
+#
+bind \cz 'fg 2>/dev/null; commandline -f repaint'
+
+# User specific environment and startup programs
+#
+fish_add_path /home/wroy/bin
+fish_add_path /home/wroy/cargo/bin
+fish_add_path /home/wroy/cargo/.dotnet/tools
