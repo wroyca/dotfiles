934f49feb8447fd54799c2763ff97cd89b61368f
William Roy <wroy@proton.me>
2024-03-24 10:57:39 -0400

    Only load treesitter explicitly

--- linux/private_dot_config/exact_nvim/exact_lua/exact_spec/exact_treesitter/readonly_treesitter.lua
+++ linux/private_dot_config/exact_nvim/exact_lua/exact_spec/exact_treesitter/readonly_treesitter.lua
@@ -1,23 +1,11 @@
 ---@type LazySpec
 return {
   [[treesitter]],
-  event = [[VeryLazy]],
-
-  -- https://github.com/neovim/neovim/pull/22923
-  --
+  cmd = [[TSInstall]],
   init = function(plugin)
     require [[lazy.core.loader]].add_to_rtp(plugin)
     require [[nvim-treesitter.query_predicates]]
   end,
-
   main = [[nvim-treesitter.configs]],
-  opts = {
-    ensure_installed = [[all]],
-    indent = {
-      enable = false
-    },
-    highlight = {
-      enable = true
-    }
-  }
+  opts = { indent = { enable = false }, highlight = { enable = false } }
 }
