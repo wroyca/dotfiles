6e97fbc5f31adba74ac18a6d0020aaf047f0049c
William Roy <wroy@proton.me>
2024-07-20 19:13:57 -0400

    Fix stylua

--- dot_config/nvim/after/ftplugin/lua.lua
+++ dot_config/nvim/after/ftplugin/lua.lua
@@ -1,6 +1,6 @@
 vim.bo.comments = [[:---,:--]]
 if vim.fn.executable [[stylua]] == 1 then
-  vim.opt_local.formatprg = [[stylua --stdin-filepath % --search-parent-directories -]]
+  vim.opt_local.formatprg = [[stylua -]]
 end
 
 vim.opt_local.include = [[\v<((do|load)file|require|reload)[^''"]*[''"]\zs[^''"]+]]
@@ -12,7 +12,7 @@ for _, path in pairs(vim.api.nvim_list_runtime_paths()) do
   vim.opt_local.path:append(vim.fs.joinpath(path --[[ @as string ]], [[lua]]))
 end
 
-local has_mini_ai, mini_ai = pcall(require, 'mini.ai')
+local has_mini_ai, _ = pcall(require, [[mini.ai]])
 if has_mini_ai then
   vim.b.miniai_config = {
     custom_textobjects = {
