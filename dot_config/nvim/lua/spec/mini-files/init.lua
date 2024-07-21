cfee9ee50e9ea553ed1bce3878cbb22d07a75071
William Roy <wroy@proton.me>
2024-07-21 14:02:37 -0400

    Start keymaps layout

--- dot_config/nvim/lua/spec/mini-files/init.lua
+++ dot_config/nvim/lua/spec/mini-files/init.lua
@@ -3,87 +3,78 @@ local M = {}
 ---@type LazyPluginSpec
 return {
   [[mini.files]], name = [[mini-files]], main = [[mini.files]], dev = true,
-  keys = {
+    keys = {
     {
       [[<leader>f]],
       function()
-        local files = require [[mini.files]]
         local file = vim.api.nvim_buf_get_name(0)
-        local file_readable = vim.fn.filereadable(file) ~= 0
+        local file_exists = vim.fn.filereadable(file) ~= 0
 
-        files.open()
-        -- files.reveal_cwd()
-        files.refresh { skip_update_cursor = true,  content = { sort = M.sort, filter = M.filter } }
+        -- The API expect a valid file system path as the anchor. Now,
+        -- `nvim_buf_get_name` doesn't validate the file system path; it
+        -- returns the buffer name. To ensure validation, `vim.fn.filereadable`
+        -- is used and if the file doesn't exists, the API will use it own
+        -- fallback.
+
+        require [[mini.files]].open(file_exists and file or nil)
+        require [[mini.files]].reveal_cwd()
+        require [[mini.files]].refresh { content = { sort = M.sort, filter = M.filter } }
       end,
       desc = [[Files]]
     }
   },
 
   opts = {
+    content = {
+      -- https://github.com/echasnovski/mini.nvim/issues/377#issuecomment-1669965688
+      sort = function(fs_entries)
+        local fs_entries_paths = table.concat(vim.iter(fs_entries):map(function(fs)
+          return fs.path
+        end):totable(), '\n')
+
+        local job = vim.fn.jobstart({ [[git]], [[check-ignore]], [[--stdin]] }, {
+          stdout_buffered = true,
+          on_stdout = function(_, out)
+            M.stdout = out
+          end
+        })
+
+        vim.fn.chansend(job, fs_entries_paths)
+        vim.fn.chanclose(job, [[stdin]])
+        vim.fn.jobwait({ job })
+
+        return require [[mini.files]].default_sort(vim.iter(fs_entries):filter(function(fs)
+          return not vim.tbl_contains(M.stdout, fs.path)
+        end):totable())
+      end,
+
+      filter = function(fs_entry)
+        return not vim.startswith(fs_entry.name, [[.]])
+      end
+    },
+
     windows = { max_number = 1 },
     options = { permanent_delete = false, use_as_default_explorer = true }
   },
 
   config = function(_, opts)
-    local show_dotfiles = true
-    local show_gitignore = true
-
-    local sort_hide = function(fs_entries)
-      local fs_entries_paths = table.concat(
-      vim
-      .iter(fs_entries)
-      :map(function(fs)
-        return fs.path
-      end)
-      :totable(),
-      "\n"
-      )
-
-      local job = vim.fn.jobstart({ [[git]], [[check-ignore]], [[--stdin]] }, {
-        stdout_buffered = true,
-        on_stdout = function(_, out)
-          M.stdout = out
-        end,
-      })
-
-      vim.fn.chansend(job, fs_entries_paths)
-      vim.fn.chanclose(job, [[stdin]])
-      vim.fn.jobwait({ job })
-
-      return require([[mini.files]]).default_sort(vim
-      .iter(fs_entries)
-      :filter(function(fs)
-        return not vim.tbl_contains(M.stdout, fs.path)
-      end)
-      :totable())
+    local toggle_state = function()
+      M.state = not M.state
+      M.sort = M.state and require [[mini.files]].default_sort or opts.content.sort
+      M.filter = M.state and require [[mini.files]].default_filter or opts.content.filter
+      require [[mini.files]].refresh {
+        content = {
+          sort = M.sort,
+          filter = M.filter
+        }
+      }
     end
 
-    local filter_show = function(fs_entry) return true end
-    local filter_hide = function(fs_entry)
-      return not vim.startswith(fs_entry.name, '.')
-    end
-
-    local toggle_gitignore = function()
-      show_gitignore = not show_gitignore
-      local new_sort = show_gitignore and require([[mini.files]]).default_sort or sort_hide
-      MiniFiles.refresh({ content = { sort = new_sort } })
-      M.sort = new_sort
-    end
-
-    local toggle_dotfiles = function()
-      show_dotfiles = not show_dotfiles
-      local new_filter = show_dotfiles and filter_show or filter_hide
-      MiniFiles.refresh({ content = { filter = new_filter } })
-      M.filter = new_filter
-      toggle_gitignore()
-    end
-
-    vim.api.nvim_create_autocmd('User', {
-      pattern = 'MiniFilesBufferCreate',
+    vim.api.nvim_create_autocmd([[User]], {
+      pattern = [[MiniFilesBufferCreate]],
       callback = function(args)
-        local buf_id = args.data.buf_id
-        vim.keymap.set('n', '.', toggle_dotfiles, { buffer = buf_id })
-      end,
+        vim.keymap.set([[n]], [[.]], toggle_state, { buffer = args.data.buf_id })
+      end
     })
 
     require [[mini.files]].setup(opts)
