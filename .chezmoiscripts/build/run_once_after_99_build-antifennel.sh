db5f95a01789a5bccaf0818cf7039efa7f62f75b
William Roy <wroy@proton.me>
2024-07-28 12:23:35 -0400

    Build and install antifennel

---
+++ .chezmoiscripts/build/run_once_after_99_build-antifennel.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+set -x # Print trace of simple commands.
+set +e # Ignore pipeline that returns a non-zero status.
+
+if command -v antifennel > /dev/null 2>&1; then
+  exit 0
+fi
+
+owd="$(pwd)"
+ret ()
+{
+  cd "$owd"
+}
+
+trap ret EXIT
+
+mkdir -p /tmp/antifennel && cd $_
+
+git init .
+git remote add --no-fetch origin https://git.sr.ht/~technomancy/antifennel --no-tags
+git config remote.origin.fetch "+refs/heads/main:refs/remotes/origin/main"
+git fetch -v --depth=1
+git pull origin main
+
+sudo make install PREFIX=/usr
+
