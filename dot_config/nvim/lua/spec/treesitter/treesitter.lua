3b49e03ee8794955d00e35751084a8564276d7d1
William Roy <wroy@proton.me>
2024-07-29 19:15:11 -0400

    Workaround treesitter modules field

--- dot_config/nvim/lua/spec/treesitter/treesitter.lua
+++ dot_config/nvim/lua/spec/treesitter/treesitter.lua
@@ -1,19 +1,67 @@
----@type LazySpec
+---@diagnostic disable: missing-fields
+---@type LazyPluginSpec
 return {
-  [[treesitter]], event = [[VeryLazy]],
+  [[nvim-treesitter/nvim-treesitter]], main = [[nvim-treesitter.configs]], event = [[VeryLazy]],
 
-  main = [[nvim-treesitter.configs]],
-  opts = {
+  ---@type TSConfig
+  opts = setmetatable({
     ensure_installed = [[all]],
-    highlight = {
-      enable = true
-    },
-    incremental_selection = {
-      enable = true,
-      keymaps = {
-        node_incremental = [[v]],
-        node_decremental = [[V]]
+    ---@type {[string]:TSModule}
+    modules = {
+      highlight = {
+        enable = false
+      },
+      incremental_selection = {
+        enable = true,
+        keymaps = {
+          node_incremental = [[v]],
+          node_decremental = [[V]]
+        }
       }
     }
-  }
+  },
+
+  -- HACK:
+  --
+  -- Use a metatable to unpack the modules table at runtime, automatically
+  -- inlining its fields into opts.
+  {
+    -- TSConfig expects a modules field, but Treesitter's internal logic also
+    -- dynamically creates it. This situation results in our modules field nesting
+    -- within Treesitter's dynamically created one. Generally, various Neovim
+    -- configurations approach this by manually inlining the configuration (e.g.):
+    --
+    -- opts = {
+    --   ---@type TSModule
+    --   highlight = {
+    --     enable = false
+    --   }
+    -- }
+    --
+    -- Although this approach is technically valid, the class annotation displays
+    -- `@field modules {[string]:TSModule}`, implying that we should configure
+    -- modules through the `modules` table. Being unaware of this internal quirk
+    -- and following this suggestion could lead to a silent merge of what seems
+    -- like a valid Lua table, resulting in a broken configuration that might go
+    -- unnoticed, particularly if other highlighting methods are layered.
+    __index = function(table, key)
+      if key == [[modules]] then
+        return nil
+      end
+      local modules = rawget(table, [[modules]])
+      if modules then
+        for k, v in pairs(modules) do
+          rawset(table, k, v)
+        end
+        rawset(table, [[modules]], nil)
+        return rawget(table, key)
+      end
+      return nil
+    end,
+    __newindex = function(table, key, value)
+      if key ~= "modules" then
+        rawset(table, key, value)
+      end
+    end
+  })
 }
