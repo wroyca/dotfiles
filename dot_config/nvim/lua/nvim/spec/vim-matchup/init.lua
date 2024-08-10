3479c2621f91044a167dffe0e3870e86d6b1bfc5
William Roy <wroy@proton.me>
2024-08-10 10:41:07 -0400

    fix(nvim): stop vim-matchup from overriding statusline

--- dot_config/nvim/lua/nvim/spec/vim-matchup/init.lua
+++ dot_config/nvim/lua/nvim/spec/vim-matchup/init.lua
@@ -2,7 +2,10 @@
 
 ---@type LazyPluginSpec
 local Spec = {
-  "andymass/vim-matchup", event = "VeryLazy"
+  "andymass/vim-matchup", event = "VeryLazy",
+  config = function(_, opts)
+    vim.g.matchup_matchparen_offscreen = {""}
+  end
 }
 
 return Spec
