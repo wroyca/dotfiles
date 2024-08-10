113ae00c06a792137f7c757e45a53238333d9f2c
William Roy <wroy@proton.me>
2024-08-10 18:49:11 -0400

    Use OSC 111 for setup_termbg_sync()

--- dot_config/nvim/lua/nvim/spec/mini/misc.lua
+++ dot_config/nvim/lua/nvim/spec/mini/misc.lua
@@ -5,11 +5,18 @@ local Spec = {
   "mini.misc", dev = true, event = "VimEnter",
 
   config = function()
-    local misc = require [[mini.misc]]
+    local misc = require "mini.misc"
     misc.setup()
     misc.setup_auto_root()
     misc.setup_restore_cursor()
-    misc.setup_termbg_sync()
+
+    -- https://github.com/echasnovski/mini.nvim/issues/1128#issuecomment-2282193831
+    misc.setup_termbg_sync = (function()
+      local sync = function() io.write(string.format("\027]11;#%06x\007", vim.api.nvim_get_hl(0, { name = "Normal" }).bg)) end
+      vim.api.nvim_create_autocmd({ "UIEnter", "ColorScheme" }, { callback = sync })
+      local reset = function() io.write('\027]111\007') end
+      vim.api.nvim_create_autocmd({ 'UILeave' }, { callback = reset })
+    end)()
   end
 }
 
