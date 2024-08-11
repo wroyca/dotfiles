63487e9958335298cab0145a008d5c2eeaa0a8a5
William Roy <wroy@proton.me>
2024-08-11 18:46:14 -0400

    Fix CoC K mapping

--- dot_config/nvim/lua/nvim/spec/coc/init.lua
+++ dot_config/nvim/lua/nvim/spec/coc/init.lua
@@ -13,10 +13,10 @@ local Spec = {
         local cw = vim.fn.expand("<cword>")
         if vim.fn.index({ "vim", "help" }, vim.bo.filetype) >= 0 then
           vim.api.nvim_command("h" .. " " .. cw)
-        elseif vim.api.nvim_eval([[coc#rpc#ready()]]) then
-          vim.fn.CocActionAsync("doHover")
+        elseif vim.api.nvim_eval("coc#rpc#ready()") and vim.fn.CocAction("hasProvider", "hover") then
+          vim.fn.CocActionAsync "doHover"
         else
-          vim.api.nvim_command("!" .. vim.o.keywordprg .. " " .. cw)
+          vim.fn.feedkeys("K", "in")
         end
       end
     },
