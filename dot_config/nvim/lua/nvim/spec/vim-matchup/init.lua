212230e538a31f06620958b95ba7654574e1b6ee
William Roy <wroy@proton.me>
2024-08-21 16:59:54 -0400

    Enable deffered matching with vim-matchup

--- dot_config/nvim/lua/nvim/spec/vim-matchup/init.lua
+++ dot_config/nvim/lua/nvim/spec/vim-matchup/init.lua
@@ -3,11 +3,15 @@
 ---@type LazyPluginSpec
 local Spec = {
   "andymass/vim-matchup", event = "VeryLazy",
+
   opts = {
-    matchparen_offscreen = {}
+    matchparen_offscreen = {},
+    matchparen_deffered = 1,
   },
+
   config = function(_, opts)
     vim.g.matchup_matchparen_offscreen = opts.matchparen_offscreen
+    vim.g.matchup_matchparen_deferred = opts.matchparen_deffered
   end
 }
 
