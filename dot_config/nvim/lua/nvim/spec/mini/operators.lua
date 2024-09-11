a71f257daea02dafd30a1b5f4f4990c9dad8254d
William Roy <wroy@proton.me>
2024-09-11 16:58:47 -0400

    fix(mini.operators): bind appropriate prefix

--- dot_config/nvim/lua/nvim/spec/mini/operators.lua
+++ dot_config/nvim/lua/nvim/spec/mini/operators.lua
@@ -5,16 +5,11 @@ local Spec = {
   "mini.operators", dev = true, opts = {},
 
   keys = {
-    { "g=",  mode = { "n", "x" }, desc = "Evalute motion" },
-    { "g==", mode = { "n", "x" }, desc = "Evalute line" },
-    { "gm",  mode = { "n", "x" }, desc = "Multiply motion" },
-    { "gmm", mode = { "n", "x" }, desc = "Multiply line" },
-    { "go",  mode = { "n", "x" }, desc = "Sort motion" },
-    { "goo", mode = { "n", "x" }, desc = "Sort line" },
-    { "gp",  mode = { "n", "x" }, desc = "Replace motion" },
-    { "gpp", mode = { "n", "x" }, desc = "Replace line" },
-    { "gs",  mode = { "n", "x" }, desc = "Exchange motion" },
-    { "gss", mode = { "n", "x" }, desc = "Exchange line" },
+    { "g=",  mode = { "n", "x" } },
+    { "gx",  mode = { "n", "x" } },
+    { "gm",  mode = { "n", "x" } },
+    { "gr",  mode = { "n", "x" } },
+    { "gs",  mode = { "n", "x" } },
   },
 }
 
