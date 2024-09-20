10f31d0cb958dcafb30663ffe94123bd156cb3b8
William Roy <wroy@proton.me>
2024-09-19 22:11:12 -0400

    feat(nvim): use OSC 52 clipboard provider

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -14,7 +14,6 @@ end)
 
 vim.g.mapleader      = " "
 vim.g.localmapleader = ","
-vim.o.clipboard      = "unnamedplus"
 vim.o.mouse          = "a"
 vim.o.mousescroll    = "ver:3,hor:0"
 vim.o.mousemoveevent = true
@@ -42,3 +41,29 @@ vim.o.smartindent    = true
 vim.o.tabstop        = 2
 vim.o.shiftwidth     = 0
 vim.o.shiftround     = true
+
+-- Nvim bundles a clipboard provider that allows copying to the system
+-- clipboard using OSC 52. OSC 52 is an Operating System Command control
+-- sequence that writes the copied text to the terminal emulator. If the
+-- terminal emulator supports OSC 52 then it will write the copied text into
+-- the system clipboard. The rational here is to avoid specific bugs and
+-- limitations related to wayland clipboard functionality, more specifically:
+--
+-- https://github.com/bugaevc/wl-clipboard/issues/12
+-- https://github.com/bugaevc/wl-clipboard/issues/210
+-- https://gitlab.gnome.org/GNOME/mutter/-/issues/524
+-- https://github.com/bugaevc/wl-clipboard/issues/206#issuecomment-1858913481
+-- https://github.com/neovim/neovim/issues/24470
+--
+vim.o.clipboard = "unnamedplus"
+vim.g.clipboard = {
+  name = 'OSC 52',
+  copy = {
+    ['+'] = require('vim.ui.clipboard.osc52').copy('+'),
+    ['*'] = require('vim.ui.clipboard.osc52').copy('*'),
+  },
+  paste = {
+    ['+'] = require('vim.ui.clipboard.osc52').paste('+'),
+    ['*'] = require('vim.ui.clipboard.osc52').paste('*'),
+  },
+}
