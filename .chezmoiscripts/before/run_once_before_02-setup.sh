1ed513681f4f6b96aae50ca676becd9282b40454
William Roy <wroy@proton.me>
2024-09-22 09:57:48 -0400

    fix(fedora): disable fractional scaling

--- .chezmoiscripts/before/run_once_before_02-setup.sh
+++ .chezmoiscripts/before/run_once_before_02-setup.sh
@@ -8,5 +8,13 @@ sudo dnf install -y xorg-x11-drv-nvidia-cuda
 sudo dnf install -y intel-media-driver
 sudo dnf install -y libva-nvidia-driver
 
+# https://github.com/rpm-software-management/dnf5/issues/138
+#
 sudo dnf group install -y \
   multimedia
+
+# This is enabled by default starting with Fedora 41. Disable it since we use
+# integer scaling, and leaving it on would unnecessarily downscale and add
+# extra computations (yes, even with integer scaling).
+#
+gsettings set org.gnome.mutter experimental-features "[]"
