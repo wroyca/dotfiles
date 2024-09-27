b0c96e84e40a8d1f9d88dd2f11b6069e36b751ed
William Roy <wroy@proton.me>
2024-09-27 09:58:28 -0400

    chore: remove emacs build script

--- .chezmoiscripts/after/run_once_after_98-setup-nautilus-directories.sh
+++ .chezmoiscripts/after/run_once_after_98-setup-nautilus-directories.sh
@@ -4,5 +4,7 @@ cd ~/ || exit
 
 [[ ! -d Projects ]] && mkdir Projects
 
+# https://gitlab.gnome.org/GNOME/nautilus/-/issues/1481
+#
 gio set Projects metadata::custom-icon \
   file:///home/wroy/.local/share/icons/morewaita/places/scalable/folder-projects.svg
