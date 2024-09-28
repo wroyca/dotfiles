9390711f215dfa4b313ef2ca6b63ffecee46318b
William Roy <wroy@proton.me>
2024-09-28 19:47:57 -0400

    Revert osc52 and switch back to wl_clipboard
    Neovim occasionally encounters issues with sending and receiving
    terminal responses. One workaround is to immediately kill and retry when
    no response is received, rather than waiting for a long timeout or
    requiring user intervention. While this reduces delays, it doesn’t
    resolve the underlying issue. There are still instances where a terminal
    response is missed when frequently pasting (e.g., every 4-5 pastes).
    
    On the other hand, wl-clipboard can cause occasional lock-ups,
    especially when key combinations like "p" + brackets are pressed
    rapidly. This creates a trade-off between minor delays during frequent
    pasting or rare lock-ups triggered by accidental key presses.
    Personally, I prefer the occasional lock-up, as it can be quickly
    resolved with a pkill -9 wl-paste command, which I've set up on my
    programmable keyboard. The frequent delays, by contrast, are more
    disruptive and lack any reasonable mitigation.
    
    https://github.com/neovim/neovim/blob/69553f7bf55c060733553d96a068c1104c885bce/runtime/lua/vim/ui/clipboard/osc52.lua#L46

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -9,11 +9,12 @@ vim.o.shada = ""
 vim.schedule(function()
   vim.o.shada = shada
   pcall(vim.cmd.rshada, { bang = true })
-  vim.cmd.doautocmd "User ShadaLoadPost"
 end)
 
-vim.g.mapleader      = " "
-vim.g.localmapleader = ","
+vim.g.mapleader      = vim.keycode("<Space>")
+vim.g.localmapleader = vim.keycode(",")
+vim.o.clipboard      = "unnamedplus"
+vim.o.guicursor      = "a:blinkwait700-blinkoff400-blinkon250-Cursor/lCursor,i-ci-ve:ver25,r-cr-o:hor20"
 vim.o.mouse          = "a"
 vim.o.mousescroll    = "ver:3,hor:0"
 vim.o.mousemoveevent = true
@@ -22,18 +23,11 @@ vim.o.list           = true
 vim.o.gdefault       = true
 vim.o.number         = true
 vim.o.cursorline     = true
-vim.o.more           = false
-vim.o.wrap           = false
-vim.o.laststatus     = 3
 vim.o.scrolloff      = 4
 vim.o.pumheight      = 8
+vim.o.laststatus     = 3
+vim.o.wrap           = false
 vim.o.fillchars      = "eob: "
-vim.o.guicursor      = "a:blinkwait700-blinkoff400-blinkon250,i-ci-ve:ver25,r-cr-o:hor20"
-
--- Most formatting and style settings will be automatically managed by
--- EditorConfig (https://editorconfig.org/), provided it is available and in
--- use.
---
 vim.o.breakindent    = true
 vim.o.copyindent     = true
 vim.o.expandtab      = true
@@ -42,29 +36,3 @@ vim.o.smartindent    = true
 vim.o.tabstop        = 2
 vim.o.shiftwidth     = 0
 vim.o.shiftround     = true
-
--- Nvim bundles a clipboard provider that allows copying to the system
--- clipboard using OSC 52. OSC 52 is an Operating System Command control
--- sequence that writes the copied text to the terminal emulator. If the
--- terminal emulator supports OSC 52 then it will write the copied text into
--- the system clipboard. The rational here is to avoid specific bugs and
--- limitations related to wayland clipboard functionality, more specifically:
---
--- https://github.com/bugaevc/wl-clipboard/issues/12
--- https://github.com/bugaevc/wl-clipboard/issues/210
--- https://gitlab.gnome.org/GNOME/mutter/-/issues/524
--- https://github.com/bugaevc/wl-clipboard/issues/206#issuecomment-1858913481
--- https://github.com/neovim/neovim/issues/24470
---
-vim.o.clipboard = "unnamedplus"
-vim.g.clipboard = {
-  name = "OSC 52",
-  copy = {
-    ["+"] = require("vim.ui.clipboard.osc52").copy("+"),
-    ["*"] = require("vim.ui.clipboard.osc52").copy("*"),
-  },
-  paste = {
-    ["+"] = require("vim.ui.clipboard.osc52").paste("+"),
-    ["*"] = require("vim.ui.clipboard.osc52").paste("*"),
-  },
-}
