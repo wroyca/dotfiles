855f609155503b8c51ad5d6ca5969c7d499b36a2
William Roy <wroy@proton.me>
2024-09-29 08:19:15 -0400

    fix(gitcommit): disable smartindent for body

--- dot_config/nvim/after/ftplugin/gitcommit.lua
+++ dot_config/nvim/after/ftplugin/gitcommit.lua
@@ -1,4 +1,7 @@
-if vim.bo.filetype ~= "gitcommit" then return end
+if vim.bo.filetype ~= "gitcommit" then
+  return
+end
 
+vim.opt_local.smartindent = false
 vim.opt_local.textwidth = 72
 vim.opt_local.formatoptions:remove({ "c", "r", "o", "q" })
