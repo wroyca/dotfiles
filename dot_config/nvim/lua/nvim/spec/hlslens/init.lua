bfe74845bbd830f019c65cf8122fd28102f8f378
William Roy <wroy@proton.me>
2024-09-29 10:23:09 -0400

    fix(hlslens): map keys manually to trigger them properly

--- dot_config/nvim/lua/nvim/spec/hlslens/init.lua
+++ dot_config/nvim/lua/nvim/spec/hlslens/init.lua
@@ -2,7 +2,17 @@
 
 ---@type LazyPluginSpec
 local Spec = {
-  "kevinhwang91/nvim-hlslens", keys = { "*", "#", "n", "N" },
+  "kevinhwang91/nvim-hlslens",
+
+  keys = function(_, keys)
+    for _, map in ipairs({ "n", "N" }) do
+      table.insert(keys, { map, ([[<cmd>execute('normal! ' . v:count1 . '%s')<cr><cmd>lua require('hlslens').start()<cr>]]):format(map), silent = true })
+    end
+    for _, map in ipairs({ "/", "*", "#", "g*", "g#" }) do
+      table.insert(keys, { map, ([[%s<cmd>lua require('hlslens').start()<cr>]]):format(map) })
+    end
+    return keys
+  end,
 
   init = function()
     vim.opt.shortmess:append("S")
