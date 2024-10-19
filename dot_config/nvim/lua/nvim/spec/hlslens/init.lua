b1f0807bbc0b7ee815a37771496c293b499d8fb0
William Roy <wroy@proton.me>
2024-10-19 13:35:00 -0400

    refactor(nvim): streamline key maps in hlslens

--- dot_config/nvim/lua/nvim/spec/hlslens/init.lua
+++ dot_config/nvim/lua/nvim/spec/hlslens/init.lua
@@ -5,12 +5,18 @@ local Spec = {
   "kevinhwang91/nvim-hlslens",
 
   keys = function(_, keys)
-    for _, map in ipairs({ "n", "N" }) do
-      table.insert(keys, { map, ([[<cmd>execute('normal! ' . v:count1 . '%s')<cr><cmd>lua require('hlslens').start()<cr>]]):format(map), silent = true })
+    local function add_key_mappings(maps, cmd_template, options)
+      for _, map in ipairs(maps) do
+        table.insert(keys, { map, cmd_template:format(map), options or {} })
+      end
     end
-    for _, map in ipairs({ "/", "*", "#", "g*", "g#" }) do
-      table.insert(keys, { map, ([[%s<cmd>lua require('hlslens').start()<cr>]]):format(map) })
-    end
+
+    local normal_cmd = [[<cmd>execute('normal! ' . v:count1 . '%s')<cr>]]
+    local hlslens_cmd = [[<cmd>lua require('hlslens').start()<cr>]]
+
+    add_key_mappings({ "n", "N" }, normal_cmd .. hlslens_cmd, { silent = true })
+    add_key_mappings({ "/", "*", "#", "g*", "g#" }, [[%s]] .. hlslens_cmd)
+
     return keys
   end,
 
--- dot_config/nvim/lua/nvim/spec/treesitter-textobjects/init.lua
+++
@@ -1,8 +0,0 @@
----@module "nvim-treesitter-textobjects"
-
----@type LazyPluginSpec
-local Spec = {
-  "nvim-treesitter/nvim-treesitter-textobjects"
-}
-
-return Spec
