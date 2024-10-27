7b7f99b6d8bc40a9c9c2f440d55582930523ea7a
William Roy <wroy@proton.me>
2024-10-27 12:22:42 -0400

    fix(nvim): update deprecated mappings

--- dot_config/nvim/lua/nvim/spec/leap/init.lua
+++ dot_config/nvim/lua/nvim/spec/leap/init.lua
@@ -1,17 +1,38 @@
 ---@module "leap"
 
----@LazyPluginSpec
+---@type LazyPluginSpec
 local Spec = {
-  "ggandor/leap.nvim", keys = { "s", "S", "gs" },
+  "ggandor/leap.nvim",
+
+  keys = {
+    "s", "S", "gs",
+
+    {
+      "r",
+      function()
+        require("leap.remote").action()
+      end,
+      mode = { "o" },
+      desc = "Leap remote",
+    },
+
+    {
+      "<C-Space>",
+      function()
+        require("leap.treesitter").select()
+      end,
+      mode = { "n", "x", "o" },
+      desc = "Leap incremental selection",
+    },
+  },
 
   opts = {
     highlight_unlabeled_phase_one_targets = true,
   },
 
   config = function(_, opts)
-    local leap = require("leap")
-    leap.setup(opts)
-    leap.add_default_mappings(true)
+    require("leap").setup(opts)
+    require("leap").create_default_mappings()
   end,
 }
 
