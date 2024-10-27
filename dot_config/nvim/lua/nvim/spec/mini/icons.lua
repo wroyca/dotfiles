013f84796ae70ed587d1251bc7a1a885faa2ce61
William Roy <wroy@proton.me>
2024-10-27 12:14:32 -0400

    docs(nvim): explain icons mock

--- dot_config/nvim/lua/nvim/spec/mini/icons.lua
+++ dot_config/nvim/lua/nvim/spec/mini/icons.lua
@@ -4,10 +4,15 @@
 local Spec = {
   "mini.icons", dev = true,
 
+  -- opts is unused, as mini modules self-export through _G.
   config = function(_, opts)
     require("mini.icons").setup(opts)
+
+    -- Mock exported functions of 'nvim-tree/nvim-web-devicons' plugin. It will
+    -- mock all its functions which return icon data by using |MiniIcons.get()|
+    -- equivalent.
     MiniIcons.mock_nvim_web_devicons()
-  end,
+  end
 }
 
 return Spec
