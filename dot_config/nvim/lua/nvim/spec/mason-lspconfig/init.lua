5ff6a925ea489f0f2a99d2bda47ba1fbc3cbcbed
William Roy <wroy@proton.me>
2024-10-28 10:40:37 -0400

    feat(nvim): partial built-in lsp support

---
+++ dot_config/nvim/lua/nvim/spec/mason-lspconfig/init.lua
@@ -0,0 +1,10 @@
+---@type LazyPluginSpec
+local Spec = {
+  "williamboman/mason-lspconfig.nvim",
+
+  opts = {
+    automatic_installation = true,
+  }
+}
+
+return Spec
---
+++ dot_config/nvim/lua/nvim/spec/mason/init.lua
@@ -0,0 +1,6 @@
+---@type LazyPluginSpec
+local Spec = {
+  "williamboman/mason.nvim", opts = {}
+}
+
+return Spec
