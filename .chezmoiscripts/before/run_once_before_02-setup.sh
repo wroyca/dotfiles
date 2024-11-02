9702d6a30d6d457fcf93cf71fbc1d0d3225f572e
William Roy <wroy@proton.me>
2024-11-02 14:06:30 -0400

    fix(multimedia): replace ffmpeg-free with ffmpeg

--- .chezmoiscripts/before/run_once_before_02-setup.sh
+++ .chezmoiscripts/before/run_once_before_02-setup.sh
@@ -5,13 +5,12 @@ sudo dnf install -y https://download1.rpmfusion.org/free/fedora/rpmfusion-free-r
 sudo dnf install -y https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-"$(rpm -E %fedora)".noarch.rpm
 sudo dnf install -y akmod-nvidia
 sudo dnf install -y xorg-x11-drv-nvidia-cuda
-sudo dnf install -y intel-media-driver
-sudo dnf install -y libva-nvidia-driver
 
-# https://github.com/rpm-software-management/dnf5/issues/138
+# https://docs.fedoraproject.org/en-US/quick-docs/installing-plugins-for-playing-movies-and-music/
 #
-sudo dnf group install -y \
-  multimedia
+sudo dnf group install -y multimedia
+sudo dnf install -y intel-media-driver
+sudo dnf swap -y ffmpeg-free ffmpeg --allowerasing
 
 # Fedora 41 uses fractional scaling by default, but we prefer to maintain
 # integer scaling. Undo this change by resetting the experimental features in
