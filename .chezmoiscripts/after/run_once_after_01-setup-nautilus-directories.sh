1c06227facbd78eaf0379909543b9e82e1c26550
William Roy <wroy@proton.me>
2024-11-03 22:48:36 -0500

    Feat(chezmoi): Reinstate prior managed configuration state

---
+++ .chezmoiscripts/after/run_once_after_01-setup-nautilus-directories.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+cd ~/ || exit
+
+[[ ! -d Projects ]] && mkdir Projects
+
+# https://gitlab.gnome.org/GNOME/nautilus/-/issues/1481
+#
+gio set Projects metadata::custom-icon \
+  file:///home/wroy/.local/share/icons/morewaita/scalable/places/folder-projects.svg
