d13b636bb96a2f69f712eba43fd9c310015b08f9
William Roy <wroy@proton.me>
2024-11-06 21:24:46 -0500

    feat(chezmoiscripts): Build emacs from source

---
+++ .chezmoiscripts/build/run_once_after_50-emacs.sh
@@ -0,0 +1,56 @@
+#!/usr/bin/env bash
+
+if command -v emacs > /dev/null 2>&1; then
+  exit 0
+fi
+
+owd="$(pwd)"
+ret ()
+{
+  cd "$owd" || exit
+}
+
+trap ret EXIT
+
+mkdir -p /tmp/emacs && cd "$_" || exit
+
+# https://dnf-plugins-core.readthedocs.io/en/latest/builddep.html
+#
+sudo dnf builddep -y emacs
+
+git init .
+git remote add --no-fetch origin https://github.com/emacs-mirror/emacs.git --no-tags
+git config remote.origin.fetch "+refs/heads/master:refs/remotes/origin/master"
+git fetch -v --depth=1
+git pull origin master
+
+CC='CFLAGS=-DMAIL_USE_LOCKF -O2 -flto=auto -ffat-lto-objects -fexceptions -g -grecord-gcc-switches -pipe -Wall -Werror=format-security -Wp,-U_FORTIFY_SOURCE,-D_FORTIFY_SOURCE=3 -Wp,-D_GLIBCXX_ASSERTIONS -specs=/usr/lib/rpm/redhat/redhat-hardened-cc1 -fstack-protector-strong -specs=/usr/lib/rpm/redhat/redhat-annobin-cc1  -m64  -mtune=generic -fasynchronous-unwind-tables -fstack-clash-protection -fcf-protection -fno-omit-frame-pointer -mno-omit-leaf-frame-pointer LDFLAGS=-Wl,-z,relro gcc'
+PKG_CONFIG_PATH=:/usr/lib64/pkgconfig:/usr/share/pkgconfig
+
+./autogen.sh
+./configure                           \
+  --prefix=/usr                       \
+  --without-pop                       \
+  --with-mailutils                    \
+  --with-pgtk                         \
+  --without-x                         \
+  --with-xwidgets                     \
+  --with-cairo                        \
+  --without-compress-install          \
+  --with-native-compilation           \
+  --with-json                         \
+  --with-dbus                         \
+  --with-gif                          \
+  --with-jpeg                         \
+  --with-png                          \
+  --with-rsvg                         \
+  --with-tiff                         \
+  --with-webp                         \
+  --with-gpm                          \
+  --with-modules                      \
+  --with-harfbuzz                     \
+  build_alias=x86_64-redhat-linux-gnu \
+  host_alias=x86_64-redhat-linux-gnu
+
+make -j$(($(nproc)+2))
+sudo make install
