9e24ced2e1abd2077b19dddc16dbf455162883b5
William Roy <wroy@proton.me>
2024-11-14 12:57:14 -0500

    Feat(qmk): Configure build system (rules.mk) for Achordion integration

--- QMK/rules.mk
+++ QMK/rules.mk
@@ -7,3 +7,12 @@ MOUSEKEY_ENABLE = no
 ORYX_ENABLE = yes
 RGBLIGHT_ENABLE = no
 SPACE_CADET_ENABLE = no
+TAP_DANCE_ENABLE = yes
+
+# Achordion is a userspace QMK library that customizes when tap-hold keys are
+# considered held vs. tapped based on the next pressed key. The library works
+# on top of QMK’s existing tap-hold implementation.
+#
+# https://getreuer.info/posts/keyboards/achordion/index.html
+#
+SRC += achordion.c
