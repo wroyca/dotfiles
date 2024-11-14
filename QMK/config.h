becc0e23a115cd5016db53b242a1e906bcbb5562
William Roy <wroy@proton.me>
2024-11-14 16:00:21 -0500

    feat(QMK): Disables hold behavior during typing streak

--- QMK/config.h
+++ QMK/config.h
@@ -1,8 +1,15 @@
 // https://docs.qmk.fm/#/config_options?id=the-configh-file
 
-#define FIRMWARE_VERSION u8"GMKyV/lmgRG"
 #define LAYER_STATE_8BIT
-#define ORYX_CONFIGURATOR
-#define RAW_USAGE_ID 0x61
-#define RAW_USAGE_PAGE 0xFF60
+#define PERMISSIVE_HOLD
+#define SERIAL_NUMBER "RmGxP/0gPx6"
 #define USB_SUSPEND_WAKEUP_DELAY 0
+
+// The ACHORDION_STREAK option disables hold behavior during a typing streak.
+//
+// In a sequence of key presses like A, B, C—where B is a tap-and-hold key and
+// A and C are regular keys—a "streak" is defined as the duration between the
+// release of A and the press of C, provided it happens within a time frame
+// shorter than the default timeout of 200 ms.
+//
+#define ACHORDION_STREAK
