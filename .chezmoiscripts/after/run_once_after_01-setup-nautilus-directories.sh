4d63a0654e96a676b9b468eef93128dca91cc31c
William Roy <wroy@proton.me>
2024-12-11 10:46:07 -0500

    Feat(chezmoiscripts): Assign custom icon for Games directory in UI

--- .chezmoiscripts/after/run_once_after_01-setup-nautilus-directories.sh
+++ .chezmoiscripts/after/run_once_after_01-setup-nautilus-directories.sh
@@ -3,8 +3,9 @@
 cd ~/ || exit
 
 [[ ! -d Projects ]] && mkdir Projects
+[[ ! -d Games ]]    && mkdir Games
 
 # https://gitlab.gnome.org/GNOME/nautilus/-/issues/1481
 #
-gio set Projects metadata::custom-icon \
-  file:///home/wroy/.local/share/icons/morewaita/scalable/places/folder-projects.svg
+gio set Projects metadata::custom-icon file:///home/wroy/.local/share/icons/morewaita/scalable/places/folder-projects.svg
+gio set Games    metadata::custom-icon file:///home/wroy/.local/share/icons/morewaita/scalable/places/folder-games.svg
