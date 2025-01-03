b477e36bdbf26e678ef5a441b1111da1c26edb7d
William Roy <wroy@proton.me>
2025-01-03 10:30:55 -0500

    Feat(chezmoiscripts): Add mingw64-gcc-c++ compiler installation support

--- .chezmoiscripts/before/run_once_before_03-setup-devel.sh
+++ .chezmoiscripts/before/run_once_before_03-setup-devel.sh
@@ -7,12 +7,13 @@ sudo dnf install -y clang-tools-extra
 sudo dnf install -y libcxx
 sudo dnf install -y libcxx-devel
 sudo dnf install -y mingw32-gcc-c++
+sudo dnf install -y mingw64-gcc-c++
 
 if ! command -v b > /dev/null 2>&1; then
  v=$(curl -sSf https://stage.build2.org/0/toolchain.sha256 | sed -n 's,^.*/build2-install-\(.*\)-stage\.sh$,\1,p')
   curl --connect-timeout 10 --retry 5 --retry-all-errors -O \
     "https://stage.build2.org/0/$v/build2-install-$v-stage.sh"
-  sh build2-install-$v-stage.sh --yes /usr
+  sh build2-install-$v-stage.sh --local --yes /usr
 fi
 
 if ! command -v git-credential-manager > /dev/null 2>&1; then
