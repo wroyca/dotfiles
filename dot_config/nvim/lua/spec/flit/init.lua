7bcfc883d35a6165e11ec07355f2f1dd278a5fc9
William Roy <wroy@proton.me>
2025-01-08 12:52:22 -0500

    fix(nvim): Pass flit.nvim keys table to which-key to include descriptions

--- dot_config/nvim/lua/spec/flit/init.lua
+++ dot_config/nvim/lua/spec/flit/init.lua
@@ -1,20 +1,30 @@
 ---@module "flit.nvim"
 
----@type LazyPluginSpec
+---@type LazyPluginSpec[]
 local Spec = {
-  "ggandor/flit.nvim", dependencies = "ggandor/leap.nvim",
+  {
+    "ggandor/flit.nvim", dependencies = "ggandor/leap.nvim",
 
-  keys = {
-    { "f", mode = { "n", "x", "o" }, desc = "Flit forward to" },
-    { "F", mode = { "n", "x", "o" }, desc = "Flit backward to" },
-    { "t", mode = { "n", "x", "o" }, desc = "Flit forward till" },
-    { "T", mode = { "n", "x", "o" }, desc = "Flit backward till" },
-  },
+    keys = {
+      { "f", mode = { "n", "x", "o" }, desc = "Flit forward to" },
+      { "F", mode = { "n", "x", "o" }, desc = "Flit backward to" },
+      { "t", mode = { "n", "x", "o" }, desc = "Flit forward till" },
+      { "T", mode = { "n", "x", "o" }, desc = "Flit backward till" },
+    },
 
-  opts = {
-    multiline = false,
-    labeled_modes = "nx",
+    opts = {
+      multiline = false,
+      labeled_modes = "nx",
+    },
   },
+  {
+    "folke/which-key.nvim",
+    opts_extend = { "spec" }
+  }
+}
+
+Spec[2].opts = {
+  spec = Spec[1].keys
 }
 
 return Spec
