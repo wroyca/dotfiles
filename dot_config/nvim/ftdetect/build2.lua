b81782178a92cb71edc45bb562b6e76156b8416f
William Roy <wroy@proton.me>
2025-01-07 23:09:19 -0500

    Feat(nvim): Migrate Build2 detection logic into pattern-based module

--- dot_config/nvim/ftdetect/build2.lua
+++ dot_config/nvim/ftdetect/build2.lua
@@ -1,16 +1,12 @@
-vim.api.nvim_create_autocmd ({ "BufRead", "BufNewFile" }, {
-  pattern = {
-    "manifest",
-    "buildfile",
-    "build2file",
-    "bootstrap.build",
-    "root.build",
+---@diagnostic disable-next-line: undefined-doc-name
+---@type vim.filetype.add
+vim.filetype.add ({
+  extension = {
+    build = "buildfile",
   },
-  callback = function ()
-    if vim.fn.expand ("<afile>") == "manifest" then
-      vim.bo.filetype = "build2-manifest"
-    else
-      vim.bo.filetype = "build2-buildfile"
-    end
-  end,
+  pattern = {
+    ["manifest"] = "manifest",
+    ["buildfile"] = "buildfile",
+    ["build2file"] = "buildfile",
+  },
 })
