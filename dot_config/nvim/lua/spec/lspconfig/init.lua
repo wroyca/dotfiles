186c2df50abbe405d7ba3bbb7d4655e7a56d39cc
William Roy <wroy@proton.me>
2025-03-03 16:56:16 -0500

    fix(nvim): replace loadfile with dofile

--- dot_config/nvim/lua/spec/lspconfig/init.lua
+++ dot_config/nvim/lua/spec/lspconfig/init.lua
@@ -9,13 +9,22 @@ local Spec = {
     "mason-lspconfig.nvim",
   },
 
-  -- FIXME: This uses a direct hack to load LSP config files by searching for
-  -- and executing them at runtime. It will lead to subtle and unpredictable
-  -- issues when used with lazy.nvim.
-  opts = {
-    clangd = loadfile (unpack (vim.api.nvim_get_runtime_file ("lsp/clangd.lua", true))) (),
-    lua_ls = loadfile (unpack (vim.api.nvim_get_runtime_file ("lsp/lua_ls.lua", true))) (),
-  },
+  opts = function ()
+    -- Helper function to safely load LSP config files
+    local function load_lsp_config (name)
+      local config_files = vim.api.nvim_get_runtime_file ("lsp/" .. name .. ".lua", true)
+      if #config_files > 0 then
+        local ok, config = pcall (dofile, config_files[1])
+        if ok then return config end
+      end
+      return nil
+    end
+
+    return {
+      clangd = load_lsp_config ("clangd"),
+      lua_ls = load_lsp_config ("lua_ls")
+    }
+  end,
 
   config = function (_, opts)
     vim.api.nvim_create_autocmd ("LspAttach", {
@@ -24,20 +33,21 @@ local Spec = {
 
         -- Helper function to create keymaps for supported LSP methods
         local function create_keymap_if_supported (method, keymap, callback, description)
-          if client:supports_method (method) then
+          if client and client:supports_method (method) then
             vim.keymap.set ("n", keymap, callback, { buffer = args.buf, desc = description })
           end
         end
 
-        create_keymap_if_supported ("textDocument/declaration",    "<leader>ld", vim.lsp.buf.declaration,     "Go to Declaration")
-        create_keymap_if_supported ("textDocument/definition",     "<leader>lD", vim.lsp.buf.definition,      "Go to Definition")
-        create_keymap_if_supported ("textDocument/implementation", "<leader>li", vim.lsp.buf.implementation,  "Go to Implementation")
-        create_keymap_if_supported ("textDocument/references",     "<leader>lr", vim.lsp.buf.references,      "Go to References")
-        create_keymap_if_supported ("textDocument/typeDefinition", "<leader>lt", vim.lsp.buf.type_definition, "Go to Type Definition")
+        create_keymap_if_supported ("textDocument/declaration",    "gd",         vim.lsp.buf.declaration,     "Go to Declaration")
+        create_keymap_if_supported ("textDocument/definition",     "gD",         vim.lsp.buf.definition,      "Go to Definition")
+        create_keymap_if_supported ("textDocument/implementation", "gi",         vim.lsp.buf.implementation,  "Go to Implementation")
+        create_keymap_if_supported ("textDocument/references",     "gr",         vim.lsp.buf.references,      "Go to References")
+        create_keymap_if_supported ("textDocument/typeDefinition", "gt",         vim.lsp.buf.type_definition, "Go to Type Definition")
         create_keymap_if_supported ("textDocument/codeAction",     "<leader>.",  vim.lsp.buf.code_action,     "Code Action")
       end,
     })
 
+    require("mason-lspconfig").setup_handlers {}
     for server_name, server_opts in pairs (opts) do
       require ("lspconfig")[server_name].setup (server_opts)
     end
