4468233aee3b2529348e864ec82d7c74794a8485
William Roy <wroy@proton.me>
2025-03-04 13:57:53 -0500

    Fix(nvim): Remove built-in DEC logic in favor of external Lumen support

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -4,12 +4,14 @@
 -- (  /  |        / | \                                    * .  ' .  .-+-
 --  \(_)_%s      /  |  \                                *   *  .   .
 
+vim.cmd.packadd ("lumen")
+
 local shada = vim.o.shada
 vim.o.shada = ""
 vim.schedule (function ()
   vim.o.shada = shada
   pcall (vim.cmd.rshada, { bang = true })
-  vim.cmd.doautocmd ("ShadaLoadPost")
+  vim.cmd.doautocmd ("User ShadaLoadPost")
 end)
 
 vim.g.mapleader      = vim.keycode ("<Space>")
@@ -34,7 +36,14 @@ vim.o.copyindent     = true
 vim.o.smartindent    = true
 vim.o.preserveindent = true
 
-for _, cmd in ipairs ({
+local function create_autocmds (cmds)
+  for _, c in ipairs (cmds) do
+    local e, d, cb = unpack (c)
+    vim.api.nvim_create_autocmd (e, { desc = d, callback = cb })
+  end
+end
+
+create_autocmds ({
   {
     { "InsertLeave", "WinEnter" },
     "Show cursor line in active window",
@@ -50,14 +59,25 @@ for _, cmd in ipairs ({
     function ()
       vim.opt_local.cursorline = false
     end,
-  }
-}) do
-  local event, desc, callback = unpack (cmd)
-  vim.api.nvim_create_autocmd (event, {
-    desc = desc,
-    callback = callback
-  })
-end
+  },
+  {
+    { "VimEnter", "VimResume", "ColorScheme" },
+    "Sync terminal background color",
+    function ()
+      if vim.g.colors_name == nil then
+        return
+      end
+      io.stdout:write (string.format ("\027]11;#%06x\007", vim.api.nvim_get_hl (0, { name = "normal" }).bg))
+    end,
+  },
+  {
+    { "VimLeavePre", "VimSuspend" },
+    "Revert terminal background color",
+    function ()
+      io.stdout:write ("\027]111;;\007")
+    end,
+  },
+})
 
 local function hi (name, opts)
   ---@diagnostic disable-next-line: deprecated
