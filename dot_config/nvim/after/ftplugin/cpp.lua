4d0df76fe699a5385e9b18205aed037de8566bd7
William Roy <wroy@proton.me>
2025-04-26 14:49:09 -0400

    chore(nvim): General maintenance

--- dot_config/nvim/after/ftplugin/cpp.lua
+++ dot_config/nvim/after/ftplugin/cpp.lua
@@ -6,8 +6,5 @@ end
 -- smartindent or cindent features. Default behavior for C-like languages
 -- includes : as a trigger key, meaning typing : (e.g., in case label or after
 -- an access modifier) will cause automatic reindentation.
---
--- For C++, this default behavior can be problematic because : is commonly used
--- in contexts where reindentation is not desirable.
---
+
 vim.opt_local.cinkeys:remove (":")
--- dot_config/nvim/ftdetect/cpp.lua
+++ dot_config/nvim/ftdetect/cpp.lua
@@ -1,13 +1,11 @@
--- Filetype detection for C++ source files
-
 ---@diagnostic disable-next-line: undefined-doc-name
 ---@type vim.filetype.add
 vim.filetype.add ({
   extension = {
-    cxx = "cpp", -- source
-    hxx = "cpp", -- header
-    ixx = "cpp", -- inline
-    txx = "cpp", -- template
-    mxx = "cpp", -- module
+    cxx = "cpp",
+    hxx = "cpp",
+    ixx = "cpp",
+    txx = "cpp",
+    mxx = "cpp",
   },
 })
