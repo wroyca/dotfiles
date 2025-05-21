75fb2095cf97465ac645e3b87f1693e0538cddb1
William Roy <wroy@proton.me>
2025-05-21 10:35:40 -0400

    Feat(config): Set global Git editor to Visual Studio Code

--- .chezmoiscripts/01-post-boot/02-packages/00-desktop/01-terminal/run_once_after_01-01-install-kitty.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-set -x # Print commands and their arguments as they are executed.
-set -e # Exit immediately if a command exits with a non-zero status.
-
-sudo dnf swap -y ptyxis kitty
-
-systemctl --user enable kitty.service
-systemctl --user start  kitty.service
