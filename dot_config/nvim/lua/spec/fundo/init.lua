4986f5956aa333b61065bc460326d0f608eec446
William Roy <wroy@proton.me>
2025-06-11 13:28:25 -0400

    chore: revert kitty removal, update mimeapps, and write nvim docs

--- dot_config/nvim/lua/spec/fundo/init.lua
+++ dot_config/nvim/lua/spec/fundo/init.lua
@@ -12,6 +12,17 @@ local Spec = {
   config = function (_, opts)
     require ("fundo").setup (opts)
 
+    -- Fundo stores undo history as individual archive files in the cache
+    -- directory. Over time, these can accumulate and consume disk space,
+    -- especially if many files are edited infrequently. To prevent this, we
+    -- proactively delete stale archives that haven't been modified in over 7
+    -- days.
+    --
+    -- Note that whe whole machinery only involves listing a single directory
+    -- and running a cheap `stat` syscall on each file. No file contents are
+    -- read, and no recursive traversal is needed. In practice, even with
+    -- hundreds of files, the entire cleanup takes less than a millisecond.
+
     local uv = vim.uv
     local archive_dir = vim.fs.joinpath (vim.fn.stdpath ("cache"), "fundo")
 
