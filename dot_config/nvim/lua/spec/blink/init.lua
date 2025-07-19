8a56fd6ee567d4dd557dc05c14e739155702df07
William Roy <wroy@proton.me>
2025-07-19 18:50:08 -0400

    Add initial specification for blink.cmp

---
+++ dot_config/nvim/lua/spec/blink/init.lua
@@ -0,0 +1,28 @@
+---@module "blink.cmp"
+
+---@type LazyPluginSpec
+local Spec = {
+  "saghen/blink.cmp", build = "cargo build --release", event = "VeryLazy",
+
+  ---@type blink.cmp.Config
+  opts = {
+    keymap = {
+      preset = "super-tab"
+    },
+    completion = {
+      menu = {
+        draw = {
+          columns = {
+            { "label", "label_description" },
+          },
+        },
+      },
+    },
+    sources = {
+      default = { "lsp", "path", "snippets", "buffer" },
+    },
+  },
+  opts_extend = { "sources.default" },
+}
+
+return Spec
