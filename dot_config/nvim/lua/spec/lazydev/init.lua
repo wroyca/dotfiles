83b007e36071391df13c61bc489bde175c9dba5f
William Roy <wroy@proton.me>
2025-07-19 18:50:10 -0400

    Add initial specification for lazydev

---
+++ dot_config/nvim/lua/spec/lazydev/init.lua
@@ -0,0 +1,40 @@
+---@module "lazydev"
+---@module "blink-cmp"
+
+---@type LazyPluginSpec[]
+local Spec = {
+  {
+    "folke/lazydev.nvim",
+    ft = "lua",
+
+    ---@type lazydev.Config
+    opts = {
+      library = {
+        { path = "luvit-meta/library", words = { "vim%.uv" } },
+      },
+    },
+  },
+  { "Bilal2453/luvit-meta" },
+
+  -- Blink completion source for require statements and module annotations.
+  {
+    "saghen/blink.cmp", optional = true,
+
+    ---@type blink.cmp.Config
+    opts = {
+      sources = {
+        default = { "lazydev", "lsp", "path", "snippets", "buffer" },
+        providers = {
+          lazydev = {
+            name = "LazyDev",
+            module = "lazydev.integrations.blink",
+            score_offset = 100,
+          },
+        },
+      },
+    },
+    opts_extend = { "sources.default", "sources.providers" },
+  }
+}
+
+return Spec
