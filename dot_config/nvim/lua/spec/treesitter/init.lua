2307a35d73978f58cc4439705e11c56eee501525
William Roy <wroy@proton.me>
2025-07-28 13:27:54 -0400

    Fix mismatch between 'filetype' and Tree-sitter parser name
    https://github.com/echasnovski/mini.nvim/issues/1908#issuecomment-3112334572

--- dot_config/nvim/lua/spec/treesitter/init.lua
+++ dot_config/nvim/lua/spec/treesitter/init.lua
@@ -19,36 +19,42 @@ local Spec = {
   },
 
   config = function (_, opts)
-    vim.iter (opts.ensure_installed):each (function (ft)
-      vim.api.nvim_create_autocmd ("FileType", {
-        pattern = { ft },
-        callback = function ()
-          require ("nvim-treesitter").install (ft) -- noop if already installed.
+    -- The 'filetype' value might be different from tree-sitter parser/language
+    -- name. We must use vim.treesitter.language.get_filetypes() to get
+    -- relevant filetypes.
+    vim.iter (opts.ensure_installed):each (function (lang)
+      local filetypes = vim.treesitter.language.get_filetypes(lang)
+      vim.iter(filetypes):each(function(ft)
+        vim.api.nvim_create_autocmd ("FileType", {
+          pattern = { ft },
+          callback = function ()
+            require ("nvim-treesitter").install (lang) -- noop if already installed.
 
-          -- On first glance, vim.treesitter.highlighter.new` looks like a
-          -- harmless constructor. In practice, it manages to chew up 130+ ms
-          -- during startup *if* Neovim is launched directly into a file (e.g.,
-          -- `nvim foo.cxx`). Not great for those of us who like snappy cold
-          -- boots.
-          --
-          -- Profiling shows the delay happens deep inside Treesitter's internal
-          -- startup path, so rather than waiting on a fix or
-          -- reverse-engineering their init sequence, we just dodge the whole
-          -- mess by deferring `treesitter.start()` to the next event loop tick.
-          --
-          -- This buys us a faster startup at the cost of one mildly annoying
-          -- side effect: Neovim may attempt a redraw while the parser is still
-          -- mid-parse, which can lead to a momentary highlight flicker.
-          --
-          -- Fortunately, this specific race condition is already being
-          -- addressed upstream: https://github.com/neovim/neovim/pull/33145
-          --
-          -- So for now, we just punt and schedule the start call.
-          vim.schedule(function()
-            pcall (vim.treesitter.start)
-          end)
-        end,
-      })
+            -- On first glance, vim.treesitter.highlighter.new` looks like a
+            -- harmless constructor. In practice, it manages to chew up 130+ ms
+            -- during startup *if* Neovim is launched directly into a file (e.g.,
+            -- `nvim foo.cxx`). Not great for those of us who like snappy cold
+            -- boots.
+            --
+            -- Profiling shows the delay happens deep inside Treesitter's internal
+            -- startup path, so rather than waiting on a fix or
+            -- reverse-engineering their init sequence, we just dodge the whole
+            -- mess by deferring `treesitter.start()` to the next event loop tick.
+            --
+            -- This buys us a faster startup at the cost of one mildly annoying
+            -- side effect: Neovim may attempt a redraw while the parser is still
+            -- mid-parse, which can lead to a momentary highlight flicker.
+            --
+            -- Fortunately, this specific race condition is already being
+            -- addressed upstream: https://github.com/neovim/neovim/pull/33145
+            --
+            -- So for now, we just punt and schedule the start call.
+            vim.schedule(function()
+              pcall (vim.treesitter.start)
+            end)
+          end,
+        })
+      end)
     end)
   end,
 }
