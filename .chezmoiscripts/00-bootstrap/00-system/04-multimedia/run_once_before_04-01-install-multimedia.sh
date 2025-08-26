70ee6f94b83ddf9237e2ea53d4dd7c13b112f07e
William Roy <wroy@proton.me>
2025-08-26 19:36:08 -0400

    Chore(chezmoiscripts): Restrict scripts to Fedora

--- .chezmoiscripts/00-bootstrap/00-system/04-multimedia/run_once_before_04-01-install-multimedia.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-set -x # Print commands and their arguments as they are executed.
-set -e # Exit immediately if a command exits with a non-zero status.
-
-sudo dnf group install -y multimedia
