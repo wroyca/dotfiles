2b18fe733a22d507c7dc3c4f9e8a351c376549a4
William Roy <wroy@proton.me>
2025-08-31 10:45:28 -0400

    Feat(emacs): Set frame parameter to undecorated in early-init.el

--- dot_config/emacs/early-init.el
+++ dot_config/emacs/early-init.el
@@ -8,9 +8,9 @@
 
 (setq package-enable-at-startup nil)
 
+(set-frame-parameter nil 'undecorated t)
 (mapc (lambda (mode) (funcall mode -1))
   '(menu-bar-mode scroll-bar-mode tool-bar-mode fringe-mode))
-
 (setq-default mode-line-format nil)
 
 ;;; early-init.el ends here
