7d17022048bc47e3aa0611c6cf013216b3db070d
William Roy <wroy@proton.me>
2025-11-10 12:59:21 -0500

    Feat(early-init): Silence loading messages during file loading operations

--- dot_config/emacs/early-init.el
+++ dot_config/emacs/early-init.el
@@ -8,6 +8,11 @@
 
 (setq package-enable-at-startup nil)
 
+(defadvice load (around quiet-loading activate)
+  "Silence loading messages during file loading operations."
+  (let ((inhibit-message t))
+    ad-do-it))
+
 (mapc (lambda (mode) (funcall mode -1))
   '(menu-bar-mode scroll-bar-mode tool-bar-mode fringe-mode))
 (setq-default mode-line-format nil)
