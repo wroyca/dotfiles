32a02366ecec66d0bd1d8e91a3454d3200bd5f6b
William Roy <wroy@proton.me>
2026-01-02 10:53:57 -0500

    feat(firefox): Disable statuspanel and tooltip

--- dot_local/share/firefoxpwa/profiles/01HW5RBHXMSDC1KQYN1DC76N8C/user.js
+++ dot_local/share/firefoxpwa/profiles/01HW5RBHXMSDC1KQYN1DC76N8C/user.js
@@ -1,6 +1,6 @@
 // Mozilla User Preferences
 
-user_pref("widget.gtk.rounded-bottom-corners.enabled", true);
 user_pref("full-screen-api.ignore-widgets", true);
+user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);
+user_pref("widget.gtk.rounded-bottom-corners.enabled", true);
 user_pref("widget.use-xdg-desktop-portal.file-picker", 1);
-
